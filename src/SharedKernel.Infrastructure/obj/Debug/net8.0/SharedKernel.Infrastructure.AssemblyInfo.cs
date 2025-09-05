98783bbc9051abafa370961cf98324bc722c947c
Alexey Savchenko <asavchenko1@eqvantalab.ru>
2025-09-05 15:51:08 +0300

    Fixed git ignore

--- src/SharedKernel.Infrastructure/obj/Debug/net8.0/SharedKernel.Infrastructure.AssemblyInfo.cs
+++ src/SharedKernel.Infrastructure/obj/Debug/net8.0/SharedKernel.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedKernel.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334649679392d8e61db98b7a84f469a5c4181bbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a46253262643b573717e54b87a125c1c49b675")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedKernel.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedKernel.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
